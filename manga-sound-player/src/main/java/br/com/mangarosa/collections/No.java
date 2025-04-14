ec10d295db83a5f9b84a59ce6a62e4d258d53bd0
bielavv <gabrielsilva82374055@gmail.com>
2025-04-14 20:24:34 -0300

    começando o código (#3)

---
+++ manga-sound-player/src/main/java/br/com/mangarosa/collections/No.java
@@ -0,0 +1,30 @@
+package main.java.br.com.mangarosa.collections;
+
+public class No {
+    private Object valor;
+    private No prox;
+
+    public No(){
+    }
+    public No(Object valor){
+        this.valor= valor;
+        this.prox = null;
+    }
+
+    public Object getValor() {
+        return valor;
+    }
+
+    public void setValor(Object valor) {
+        this.valor = valor;
+    }
+
+    public No getProx() {
+        return prox;
+    }
+
+    public void setProx(No prox) {
+        this.prox = prox;
+    }
+}
+
