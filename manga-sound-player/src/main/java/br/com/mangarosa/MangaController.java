13445182b8228ba23088f0e328991edcb95d38ad
bielavv <gabrielsilva82374055@gmail.com>
2025-04-18 04:19:36 -0300

    Implementação da classe musica (#4)

---
+++ manga-sound-player/src/main/java/br/com/mangarosa/MangaController.java
@@ -0,0 +1,10 @@
+package main.java.br.com.mangarosa;
+
+public class MangaController {
+    private ListaEncadeada repositorioMusica;
+    private ListaEncadeada artistas;
+
+    public void adicionarMusica(String titulo, String path, String nomeArtista){
+    }
+
+}
